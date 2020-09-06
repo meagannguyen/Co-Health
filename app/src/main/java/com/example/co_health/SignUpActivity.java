cb8c496e9c69402d43c18e4a300b36c5a87f18a8
jquitgra <45401768+jquitgra@users.noreply.github.com>
2020-09-06 02:49:15 -0400

    finished signup validation logic

--- app/src/main/java/com/example/co_health/SignUpActivity.java
+++ app/src/main/java/com/example/co_health/SignUpActivity.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
     EditText emailIn, userIn, pwdIn, pwdIn2;
     Button createAccountButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +44,18 @@ public class SignUpActivity extends AppCompatActivity {
             emailIn.setError(null);
             return true;
         }
+    }
+
+    private boolean validatePwds(){
+        String p1 = pwdIn.getEditableText().toString().trim();
+        String p2 = pwdIn2.getEditableText().toString().trim();
+        if(p1.isEmpty()){
+            pwdIn.setError("Please enter a password");
+            return false;
+        }else if(p2.isEmpty()){
+            pwdIn2.setError("Please enter your password again");
+            return false;
+        }
+        return pwdIn2.equals(pwdIn2);
     }
 }
